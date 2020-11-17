d11db0b71ef25d6d42974c0fc52a74b14bc24991
Denis <Denis@DENISPC>
2020-11-17 19:12:57 +0300

    .

--- Задание 7/Задание 7.cpp
+++ Задание 7/Задание 7.cpp
@@ -4,14 +4,15 @@ int main()
 {
     setlocale(NULL, "rus");
     int mass[2][4];
-    int n;
+    int n = 1;
     int pr = 1;
     for (int i = 0; i < 2; i++)
     {
-        for (int j = 0; j < 4; j++)
-        {
-            mass[i][j] = rand()  % 3 + 1;
+        for (int j = 0; j < 4; j++) {
+            cout << "Введите " << n << "-й элемент" << endl;
+            cin >> mass[i][j];
             pr *= mass[i][j];
+            n++;
         }
     }
     for (int i = 0; i < 2; i++)
